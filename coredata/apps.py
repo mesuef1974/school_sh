16d2b0955b4845c38728744e63f15be0d1e66951
Your Name <you@example.com>
2026-02-11 17:58:43 +0300

    debug: Simplify coredata/apps.py to its bare minimum

--- coredata/apps.py
+++ coredata/apps.py
@@ -1,6 +1,6 @@
 
 from django.apps import AppConfig
 
-class CoreDataConfig(AppConfig):
+class CoredataConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'coredata'
